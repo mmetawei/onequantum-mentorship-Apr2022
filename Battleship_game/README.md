e2523fdca36192572956b301868fd7c7cd2942e0
mmetawei <36227778+mmetawei@users.noreply.github.com>
2022-06-16 13:00:34 +0200

    Add files via upload

---
+++ Battleship_game/README.md
@@ -0,0 +1,2 @@
+#Battleship Board Game <br>
+This package will count the number of battleships on the board, no adjacent battleships are allowed.
---
+++ Battleship_game/src/battleship_board/battelship.py
@@ -0,0 +1,12 @@
+class Solution:
+    def countBattleships(self, board: List[List[str]]) -> int:
+        if len(board) ==0: return 0
+        m = len(board)
+        n = len(board[0])
+        counts=0
+        for row in range(0, m):
+            for col in range(0, n):
+                if board[row][col] == 'X' and (row == 0 or board[row-1][col] == '.') and (col == 0 or board[row][col-1] == '.'):
+                    counts += 1
+        return counts
+                        
